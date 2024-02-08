d8de8333a0aac58f4a7c081f8e28cf63810130c0
Joao Victor <joaovictoroliveira1216@gmail.com>
2024-02-08 19:19:24 -0300

    Commit inicial

---
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/controllers/StudentController.java
@@ -0,0 +1,35 @@
+package com.rocketseat.certification_nlw.modules.students.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rocketseat.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import com.rocketseat.certification_nlw.modules.students.useCases.VerifyIfHasCertificationUseCase;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private VerifyIfHasCertificationUseCase VerifyIfHasCertificationUseCase;
+    
+    @PostMapping("/verifyIfHasCertification")
+    public String verifyIfHasCertification(@RequestBody VerifyHasCertificationDTO VerifyHasCertificationDTO){
+        System.out.println(VerifyHasCertificationDTO);
+
+        var result = this.VerifyIfHasCertificationUseCase.execute(VerifyHasCertificationDTO);
+
+        if (result) {
+            return "usuario nao pode fazer";
+        }
+        return "usuario pode fazer";
+    };
+
+}
